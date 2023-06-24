11033fa110ebb689d7cb40da154da8d0e522666f
Mahima Tamta <mhtamta5@gmail.com>
2023-06-24 16:22:50 +0530

    integrating morgan and helmet task20

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const userRouter = require('./router/usersignup')
 const purchaseRouter = require('./router/purchase')
 const passeord = require('./router/recpassword')
 const premiumFeatures = require('./router/premium_features')
+const  helmet = require('helmet')
+const morgan = require('morgan');
+const fs = require('fs')
 
 // const premiumFeatures= require('./router/premiumFeatures')
 const sequel = require('./util/userdatabase');
@@ -28,6 +31,9 @@ app.use('/premiumfeatures', premiumFeatures);
 
 
 app.use(cors());
+app.use(helmet());
+const logstream = fs.createWriteStream(path.join(__dirname, './error.log'), {flags: 'a'});
+app.use(morgan('combined', {stream: logstream}));
 
 userlist.hasMany(expenselist);
 expenselist.belongsTo(userlist);
@@ -42,9 +48,15 @@ Order.belongsTo(userlist);
 
 userlist.hasMany(DownloadedFiles);
 DownloadedFiles.belongsTo(userlist)
+app.use((err, req, res, next) => {
+    // Log the error
+    console.error(err);
+    next(err);
+  });
 
 sequel.sync().then(result => {
     app.listen(3000);
+    
 })
 .catch(err => {
     console.log(err);
